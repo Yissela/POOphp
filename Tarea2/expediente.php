dcafbc22fb7c9e9c685f90e26d0b682a502ec46c
unknown <iliany.yiga@gmail.com>
2024-04-19 23:38:44 -0600

    Error en la base de datos

--- Tarea2/expediente.php
+++ Tarea2/expediente.php
@@ -28,4 +28,8 @@
   <div class="col-12">
     <button type="submit" class="btn btn-primary">Guardar</button>
   </div>
+
+
 </form>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
